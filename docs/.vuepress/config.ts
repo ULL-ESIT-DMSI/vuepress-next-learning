872b135fc0c57d165ce8ea850f558b96eb5fac66
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-09-06 13:44:23 +0100

    learning vuepress@next

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,24 +3,6 @@ import { defaultTheme, } from '@vuepress/theme-default'
 import { searchPlugin } from '@vuepress/plugin-search'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import * as path from 'path'
-
-/*
-const navBar2 = [
-  // NavbarItem
-  {
-    text: 'Resources',
-    link: '/pages/resources',
-  },
-  // NavbarGroup
-  {
-    text: 'Group',
-    children: ['/group/foo.md', '/group/bar.md'],
-  },
-  // string - page file path
-  '/bar/README.md',
-]
-*/
-
 import { navigationBar } from './navigation-bar'
 
 export default defineUserConfig({
